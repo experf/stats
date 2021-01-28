57f174e4aebd937d01b45419b7fd072f674669df
nrser <neil@neilsouza.com>
2021-01-28 22:10:29 +0800

    Start testing Open Graph metadata in Link #1

---
+++ umbrella/apps/cortex/test/cortex/trackers/link/open_graph_metadata_test.exs
@@ -0,0 +1,36 @@
+defmodule Cortex.Trackers.Link.OpenGraphMetadataTest do
+  use Cortex.DataCase
+
+  import Cortex.AccountsFixtures
+  alias Cortex.Trackers
+
+  describe "link.open_graph_metadata" do
+    alias Cortex.Trackers.Link
+    alias Cortex.OpenGraph
+
+    test "create_link/1 with normalized Open Graph metadata creates a link" do
+      attrs = %{
+        "destination_url" => "http://stats.test/link/destination_url",
+        "open_graph_metadata" => %{
+          "og:title" => "What Up",
+          "og:image" => [
+            %{
+              "url" => "http://stats.test/blah",
+              "alt" => "BLaH!"
+            }
+          ],
+        }
+      }
+
+      assert {:ok, %Link{} = link} = Trackers.create_link(user_fixture(), attrs)
+
+      assert link.open_graph_metadata."og:title" == "What Up"
+      assert is_list(link.open_graph_metadata."og:image")
+      assert is_map(link.open_graph_metadata."og:image" |> List.first())
+
+      assert link.open_graph_metadata."og:image"
+             |> List.first()
+             |> Map.get(:url) == "http://stats.test/blah"
+    end
+  end
+end
