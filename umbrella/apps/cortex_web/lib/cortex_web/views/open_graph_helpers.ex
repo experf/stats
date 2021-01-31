be240d1555ce32be0c6897fa49b2d12c81d6da15
nrser <neil@neilsouza.com>
2021-02-01 03:14:27 +0800

    Cleaned up og meta tag generation

--- umbrella/apps/cortex_web/lib/cortex_web/views/open_graph_helpers.ex
+++ umbrella/apps/cortex_web/lib/cortex_web/views/open_graph_helpers.ex
@@ -4,113 +4,61 @@ defmodule CortexWeb.OpenGraphHelpers do
   alias Cortex.OpenGraph
   alias CortexWeb.EtcHelpers
 
-  def render_open_graph_meta_tag(property, content) do
-    case tag(:meta, property: property, content: content) do
-      {:safe, content} -> "#{content}\n"
-    end
+  def open_graph_meta_tag(property, content) do
+    tag(:meta, property: property, content: content)
   end
 
-  def render_open_graph_image(x, acc) when is_nil(x), do: acc
+  defp reduce_sub_struct(_, value, acc) when is_nil(value), do: acc
 
-  def render_open_graph_image(list, acc) when is_list(list) do
-    list |> Enum.reduce(acc, &render_open_graph_image/2)
+  defp reduce_sub_struct(key, value, acc) when is_list(value) do
+    Enum.reduce(
+      value,
+      acc,
+      fn entry, acc -> reduce_sub_struct(key, entry, acc) end
+    )
   end
 
-  def render_open_graph_image(image, acc) when is_map(image) do
-    acc =
-      image
-      |> Enum.reduce(
-        acc,
-        fn {key, value}, acc ->
-          case key do
+  defp reduce_sub_struct(key, value, acc)
+       when is_atom(key) and is_map(value) do
+    [
+      open_graph_meta_tag(Atom.to_string(key), value |> Map.get("url"))
+      | value
+        |> Enum.reduce(acc, fn {k, v}, acc ->
+          case k do
             "url" -> acc
-            key -> [render_open_graph_meta_tag("og:image:#{key}", value) | acc]
+            k -> [open_graph_meta_tag("#{key}:#{k}", v) | acc]
           end
-        end
-      )
-
-    [render_open_graph_meta_tag("og:image", image["url"]) | acc]
+        end)
+    ]
   end
 
-  def render_open_graph_audio(x, acc) when is_nil(x), do: acc
+  def open_graph_meta_tags(x) when is_nil(x), do: [] # EtcHelpers.maybe(x)
 
-  def render_open_graph_audio(list, acc) when is_list(list) do
-    list |> Enum.reduce(acc, &render_open_graph_audio/2)
-  end
-
-  def render_open_graph_audio(audio, acc) when is_map(audio) do
-    acc =
-      audio
-      |> Enum.reduce(
-        acc,
-        fn {key, value}, acc ->
-          case key do
-            "url" -> acc
-            key -> [render_open_graph_meta_tag("og:audio:#{key}", value) | acc]
-          end
-        end
-      )
-
-    [render_open_graph_meta_tag("og:audio", audio.url) | acc]
-  end
-
-  def render_open_graph_video(nil = _, acc), do: acc
-
-  def render_open_graph_video(videos, acc) when is_list(videos) do
-    videos |> Enum.reduce(acc, &render_open_graph_video/2)
-  end
-
-  def render_open_graph_video(video, acc) when is_map(video) do
-    acc =
-      video
-      |> Enum.reduce(
-        acc,
-        fn {key, value}, acc ->
-          case key do
-            "url" -> acc
-            key -> [render_open_graph_meta_tag("og:video:#{key}", value) | acc]
-          end
-        end
-      )
-
-    [render_open_graph_meta_tag("og:video", video.url) | acc]
-  end
-
-
-  def render_open_graph_metadata(x) when is_nil(x), do: EtcHelpers.maybe(x)
-
-  def render_open_graph_metadata(%OpenGraph.Metadata{} = metadata) do
+  def open_graph_meta_tags(%OpenGraph.Metadata{} = metadata) do
     metadata
     |> Map.from_struct()
-    |> Enum.reduce(
-      [],
-      fn {key, value}, acc ->
-        case {key, value} do
-          {:__struct__, _} ->
-            acc
+    |> Enum.reduce([], fn {key, value}, acc ->
+      case {key, value} do
+        {:__struct__, _} ->
+          acc
 
-          {_, nil} -> acc
+        {_, nil} ->
+          acc
 
-          {:"og:image", value} ->
-            render_open_graph_image(value, acc)
+        {key, value} when key in [:"og:image", :"og:audio", :"og:video"] ->
+          reduce_sub_struct(key, value, acc)
 
-          {:"og:audio", value} ->
-            render_open_graph_audio(value, acc)
-
-          {:"og:video", value} ->
-            render_open_graph_video(value, acc)
-
-          {key, value} ->
-            [
-              render_open_graph_meta_tag(
-                Atom.to_string(key),
-                to_string(value)
-              )
-              | acc
-            ]
-        end
+        {key, value} ->
+          [open_graph_meta_tag(Atom.to_string(key), to_string(value)) | acc]
       end
-    )
-    |> (fn acc -> content_tag :pre, acc end).()
+    end)
+  end
+
+  def open_graph_meta_tags_dump(%OpenGraph.Metadata{} = metadata) do
+    metadata
+    |> open_graph_meta_tags()
+    |> Enum.map(&safe_to_string/1)
+    |> Enum.join("\n")
+    |> (fn dump -> content_tag(:pre, dump) end).()
   end
 end
