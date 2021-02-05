53786855f17746a8a7a1074fbb3f3b242cfa194e
nrser <neil@neilsouza.com>
2021-02-05 22:13:00 +0800

    CLI iex bug

--- cli/stats/cmd/iex.py
+++ cli/stats/cmd/iex.py
@@ -7,23 +7,23 @@ def add_to(subparsers):
         help="Elixir REPL, run in the mix environment",
     )
     parser.add_argument(
-        "argv",
+        "args",
         nargs="*",
         help=(
             "Arguments to pass to `mix`, such as `phx.server` to run the\n"
             "Phoenix server in the `iex` console."
         ),
     )
-    parser.set_defaults(func=lambda args: run(**args.__dict__))
+    parser.set_defaults(func=run)
 
-def run(argv=tuple(), **_opts):
+def run(args=tuple(), **_opts):
     sh.replace(
         "iex",
         {
             "erl": "-kernel shell_history enabled",
             "dot-iex": cfg.paths.DEV / ".iex.exs",
         },
-        "-S", "mix", *argv,
+        "-S", "mix", *args,
         chdir=cfg.paths.REPO / "umbrella",
         opts_style=" ",
     )
