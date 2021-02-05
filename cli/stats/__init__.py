1faeecf338d575e921c900de9b575e8eb63c6739
nrser <neil@neilsouza.com>
2021-02-05 13:45:25 +0800

    Move `stats cortex mix` -> `stats mix`
    Getting annoyed of typing it out already I guess...

--- cli/stats/__init__.py
+++ cli/stats/__init__.py
@@ -6,7 +6,7 @@ import argparse
 
 import argcomplete
 
-from . import log as logging, cortex, kafka, docker, dev
+from . import log as logging, cortex, kafka, docker, dev, mix
 from .io import ERR
 
 LOG = logging.getLogger(__name__)
@@ -39,7 +39,7 @@ class ArgumentParser(argparse.ArgumentParser):
 def make_parser() -> ArgumentParser:
     parser = ArgumentParser()
     subparsers = parser.add_subparsers(help="Select a command")
-    for cmd in (cortex, docker, kafka, dev):
+    for cmd in (cortex, dev, docker, kafka, mix):
         cmd.add_to(subparsers)
     return parser
 
